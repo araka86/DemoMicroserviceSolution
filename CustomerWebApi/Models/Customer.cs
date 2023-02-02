3264a8faa43349855531030ce3cc81c0a6e4e11e
Ruslan <araka86@gmail.com>
2023-02-02 02:56:27 +0200

    Добавьте файлы проекта.

---
+++ CustomerWebApi/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerWebApi.Models
+{
+    [Table("customer", Schema = "dbo")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("customer_id")]
+        public int CustomerId { get; set; }
+        [Column("customer_name")]
+        public string CustomerName { get; set; } 
+        [Column("customer_phone")]
+        public string CustomerPhone { get; set; } 
+        [Column("customer_email")]
+        public string CustomerEmail { get; set; } 
+
+
+    }
+}
---
+++ CustomerWebApi/Program.cs
@@ -0,0 +1,38 @@
+using CustomerWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbname = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+
+//var dbHost = "localhost";
+//var dbname = "dbm_customer";
+//var dbPassword = "123!@#QWEqwe";
+var connectionString = $"Data Source={dbHost}; Initial Catalog={dbname};User ID=sa; Password={dbPassword}; TrustServerCertificate=true";
+
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+
+//builder.Services.AddDbContext<CustomerDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultStringConnections"));
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
